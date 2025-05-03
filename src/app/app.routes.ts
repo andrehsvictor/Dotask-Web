56c26f52315bd3ff025669ec8343b4489b8a93e1
andrehsvictor <andrehsvictor@gmail.com>
2025-05-03 08:35:56 -0300

    feat: add routes for VerifyEmail and ResetPassword components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
+import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,13 @@ export const routes: Routes = [
     {
         path: 'signup',
         component: SignupComponent,
-    }
+    },
+    {
+        path: 'verify-email',
+        component: VerifyEmailComponent,
+    },
+    {
+        path: 'reset-password',
+        component: ResetPasswordComponent
+    },
 ];
