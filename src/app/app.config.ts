9a94abd55d869cfdd510ac5e50e0fe67b0f6f84a
andrehsvictor <andrehsvictor@gmail.com>
2025-05-03 10:25:28 -0300

    feat: enhance app configuration to include HTTP interceptors for token management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+  provideHttpClient(
+    withInterceptorsFromDi()
+  ),
   {
     provide: HTTP_INTERCEPTORS,
     useClass: TokenInterceptorService,
